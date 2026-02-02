816a06844395f387b0205ebbd887c510e1a55c0b
Wesley Triches <wesleygtriches@gmail.com>
2026-02-02 13:41:42 -0300

    Consulta com varios criterios

--- src/main/java/com/wesley/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wesley/workshopmongo/config/Instantiation.java
@@ -44,8 +44,8 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 
 		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/08/2025"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/08/2018"), new AuthorDTO(bob));
-		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/08/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/08/2025"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/08/2025"), new AuthorDTO(alex));
 
 		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().addAll(Arrays.asList(c3));
